ad59cbe9a1b9f8ebdc3a7d7cca169feb3e02961b
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-21 22:22:48 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -2,27 +2,67 @@
 #include <random>
 using namespace std;
 
-template <typename T>
-void fill_arr (T arr[], int size) {
-	random_device rd;
-	mt19937 gen(rd());
 
-	if (typeid(T) == typeid(float))
-		uniform_real_distribution<float> dis(0, 100);
-	else if (typeid(T) == typeid(double))
-		uniform_real_distribution<double> dis(1.0, 100.0);
-	else
-		uniform_int_distribution<int> dis(0, 100);
-	
-	for (int i = 0; i < size; i++) {
-		arr[i] = dis(gen);
-	}
+template <typename T>
+void fill_array(T* arr, int size) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(-20, 20);
+    for (int i = 0; i < size; i++) {
+        arr[i] = dist(gen);
+    }
+}
+
+
+template <typename T>
+void find_negative_positions(T* arr, int size, int& left_index, int& right_index) {
+    left_index = -1;
+    right_index = -1;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            if (left_index == -1) {
+                left_index = i;
+            }
+            right_index = i;
+        }
+    }
 }
 
 template <typename T>
-int
+void sort_arr(T* arr, int left_index, int right_index) {
+	for (int i = left_index; i < right_index; i++) {
+		for (int j = i + 1; j <= right_index; j++) {
+			if (arr[i] > arr[j]) {
+				swap(arr[i], arr[j]);
+			}
+		}
+	}
+}
 
 int main() {
+    const int size = 20;
+    int arr[size];
 
-	return 0;
+    fill_array(arr, size);
+
+    cout << "Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int left_index, right_index;
+    find_negative_positions(arr, size, left_index, right_index);
+    cout << "left negative index: " << left_index << endl;
+    cout << "right negative index: " << right_index << endl;
+
+    sort_arr(arr, left_index, right_index);
+
+    cout << "sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
